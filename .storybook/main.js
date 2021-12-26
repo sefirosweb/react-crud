867f3782e92bbe65f890a2b51f2188dac7d287cd
Javier García <javier.garcia@decowood.es>
2021-12-26 18:05:11 +0100

    installed sass dependencies

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 module.exports = {
   "stories": [
     "../src/**/*.stories.mdx",
@@ -5,6 +7,41 @@ module.exports = {
   ],
   "addons": [
     "@storybook/addon-links",
-    "@storybook/addon-essentials"
-  ]
+    "@storybook/addon-essentials",
+    "@storybook/preset-scss"
+  ],
+
+  webpackFinal: async (config, { configType }) => {
+    const ruleCssIndex = config.module.rules.findIndex(
+      (rule) => rule.test.toString() === "/\\.css$/"
+    );
+
+    config.module.rules[ruleCssIndex].use.map((item) => {
+      if (item.loader && item.loader.includes("/css-loader/")) {
+        item.options.modules = {
+          mode: "local",
+          localIdentName:
+            configType === "PRODUCTION"
+              ? "[local]__[hash:base64:5]"
+              : "[name]__[local]__[hash:base64:5]",
+        };
+      }
+
+      return item;
+    });
+
+    return config;
+  }
+
+
+
+  // webpackFinal: async (config) => {
+  //   config.module.rules.push({
+  //     test: /\.scss$/,
+  //     use: ['style-loader', 'css-loader', 'sass-loader'],
+  //     include: path.resolve(__dirname, "../")
+  //   });
+
+  //   return config;
+  // }
 }
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,4 @@
-
+import '../sass/app.scss'
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
