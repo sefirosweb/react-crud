8dcb9faade9e4a1bb49c45cf62e9d2e0e7176922
Javier García <javier.garcia@decowood.es>
2021-12-26 20:07:27 +0100

    Add action

--- src/stories/Buttons.stories.js
+++ src/stories/Buttons.stories.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
 import { CancelButton } from '../components/buttons/CancelButton';
 import { CronjobButton } from '../components/buttons/CronjobButton';
 import { DeleteButton } from '../components/buttons/DeleteButton';
@@ -7,32 +8,37 @@ import { EditButton } from '../components/buttons/EditButton';
 import { LoadingButton } from '../components/buttons/LoadingButton';
 import { RefreshButton } from '../components/buttons/RefreshButton';
 
+export const actions = {
+    onClick: action('onClick')
+};
+
+
 const CancelButtonStory = storiesOf('Buttons/CancelButton', module);
 CancelButtonStory.add('Primary', () => {
-    return (<CancelButton />);
+    return (<CancelButton {...actions} />);
 });
 
 const CronjobButtonStory = storiesOf('Buttons/CronjobButton', module);
 CronjobButtonStory.add('Primary', () => {
-    return (<CronjobButton />);
+    return (<CronjobButton {...actions} />);
 });
 
 const DeleteButtonStory = storiesOf('Buttons/DeleteButton', module);
 DeleteButtonStory.add('Primary', () => {
-    return (<DeleteButton />);
+    return (<DeleteButton  {...actions} />);
 });
 
 const EditButtonStory = storiesOf('Buttons/EditButton', module);
 EditButtonStory.add('Primary', () => {
-    return (<EditButton />);
+    return (<EditButton  {...actions} />);
 });
 
 const LoadingButtonStory = storiesOf('Buttons/LoadingButton', module);
 LoadingButtonStory.add('Primary', () => {
-    return (<LoadingButton />);
+    return (<LoadingButton {...actions} />);
 });
 
 const RefreshButtonStory = storiesOf('Buttons/RefreshButton', module);
 RefreshButtonStory.add('Primary', () => {
-    return (<RefreshButton />);
+    return (<RefreshButton  {...actions} />);
 });
