2d3b1c7e30c6b19536f0c0fbc367d771b8bb1565
Javier García <javier.garcia@decowood.es>
2021-12-28 18:01:46 +0100

    Fixed mock

--- src/stories/InputDataField.stories.js
+++ src/stories/InputDataField.stories.js
@@ -11,5 +11,5 @@ const Template = (args) => <InputDataField {...args} />
 
 export const Default = Template.bind({});
 Default.args = {
-    url: '/api/crud/options'
+    url: '/api/getSelectOptions'
 };
