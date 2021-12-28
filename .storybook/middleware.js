fc0c969e69c344c855fe06629f929208c3b3a09f
Javier García <javier.garcia@decowood.es>
2021-12-28 16:27:27 +0100

    Added time on response MOCK request

--- .storybook/middleware.js
+++ .storybook/middleware.js
@@ -1,6 +1,21 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+// Is for MOCK the requests
+
+
+const makeRandomText = (length) => {
+    let result = '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789 ';
+    const charactersLength = characters.length;
+    for (var i = 0; i < length; i++) {
+        result +=
+            characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+}
+
+const TIMEOUT = 800
 
 const expressMiddleWare = router => {
     router.use(bodyParser.urlencoded({ extended: false }));
@@ -18,23 +33,51 @@ const expressMiddleWare = router => {
         })
     })
 
+    router.get("/api/crud", (request, response) => {
+        const random = Math.floor(Math.random() * 300)
+        const data = []
+        for (var i = 0; i < random; i++) {
+            data.push(
+                {
+                    "id": makeRandomText(5),
+                    "ean": makeRandomText(8),
+                    "sku_base": makeRandomText(9),
+                    "descripcion": makeRandomText(50),
+                }
+            )
+        }
+
+        setTimeout(() => {
+            response.send({
+                success: true,
+                message: "Data inserted correctly",
+                data
+            })
+        }, TIMEOUT)
+    })
     router.post("/api/crud", (request, response) => {
-        response.send({
-            success: true,
-            message: "Data inserted correctly"
-        })
+        setTimeout(() => {
+            response.send({
+                success: true,
+                message: "Data inserted correctly"
+            })
+        }, TIMEOUT)
     })
     router.put("/api/crud", (request, response) => {
-        response.send({
-            success: true,
-            message: "Data updated correctly"
-        })
+        setTimeout(() => {
+            response.send({
+                success: true,
+                message: "Data updated correctly"
+            })
+        }, TIMEOUT)
     })
     router.delete("/api/crud", (request, response) => {
-        response.send({
-            success: true,
-            message: "Data deleted correctly"
-        })
+        setTimeout(() => {
+            response.send({
+                success: true,
+                message: "Data deleted correctly"
+            })
+        }, TIMEOUT)
     })
 };
 
