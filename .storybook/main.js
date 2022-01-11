68a78aa88e84951e27eca04cba51cc4eea407b3c
Javier García <javier.garcia@decowood.es>
2022-01-11 11:30:39 +0000

    Update dependencies

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,47 +1,46 @@
-const path = require('path');
+const path = require('path')
 
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/preset-scss"
-  ],
+    stories: [
+        '../src/**/*.stories.mdx',
+        '../src/**/*.stories.@(js|jsx|ts|tsx)',
+    ],
+    addons: [
+        '@storybook/addon-links',
+        '@storybook/addon-essentials',
+        '@storybook/preset-scss',
+        '@storybook/addon-postcss',
+    ],
 
-  webpackFinal: async (config, { configType }) => {
-    const ruleCssIndex = config.module.rules.findIndex(
-      (rule) => rule.test.toString() === "/\\.css$/"
-    );
+    webpackFinal: async (config, { configType }) => {
+        const ruleCssIndex = config.module.rules.findIndex(
+            (rule) => rule.test.toString() === '/\\.css$/'
+        )
 
-    config.module.rules[ruleCssIndex].use.map((item) => {
-      if (item.loader && item.loader.includes("/css-loader/")) {
-        item.options.modules = {
-          mode: "local",
-          localIdentName:
-            configType === "PRODUCTION"
-              ? "[local]__[hash:base64:5]"
-              : "[name]__[local]__[hash:base64:5]",
-        };
-      }
+        config.module.rules[ruleCssIndex].use.map((item) => {
+            if (item.loader && item.loader.includes('/css-loader/')) {
+                item.options.modules = {
+                    mode: 'local',
+                    localIdentName:
+                        configType === 'PRODUCTION'
+                            ? '[local]__[hash:base64:5]'
+                            : '[name]__[local]__[hash:base64:5]',
+                }
+            }
 
-      return item;
-    });
+            return item
+        })
 
-    return config;
-  }
+        return config
+    },
 
+    // webpackFinal: async (config) => {
+    //   config.module.rules.push({
+    //     test: /\.scss$/,
+    //     use: ['style-loader', 'css-loader', 'sass-loader'],
+    //     include: path.resolve(__dirname, "../")
+    //   });
 
-
-  // webpackFinal: async (config) => {
-  //   config.module.rules.push({
-  //     test: /\.scss$/,
-  //     use: ['style-loader', 'css-loader', 'sass-loader'],
-  //     include: path.resolve(__dirname, "../")
-  //   });
-
-  //   return config;
-  // }
+    //   return config;
+    // }
 }
