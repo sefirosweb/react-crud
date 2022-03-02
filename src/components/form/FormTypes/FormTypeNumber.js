dad0cab57f471bbcff8e1d90492b27a283d26832
Sefirosweb <sefirosweb@gmail.com>
2022-03-02 20:40:11 +0000

    Added FormTypeNumber

---
+++ src/components/form/FormTypes/FormTypeNumber.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import { Form } from 'react-bootstrap'
+import PropTypes from 'prop-types'
+
+const FormTypeNumber = ({
+    type,
+    inputFieldName,
+    isLoading,
+    label,
+    value,
+    handleChange,
+    className,
+}) => {
+    return (
+        <Form.Group controlId={inputFieldName} className={className}>
+            <Form.Label>{label}</Form.Label>
+            <Form.Control
+                type={type}
+                readOnly={isLoading}
+                name={inputFieldName}
+                onChange={handleChange}
+                value={value}
+            />
+        </Form.Group>
+    )
+}
+
+FormTypeNumber.propTypes = {
+    type: PropTypes.number.isRequired,
+    inputFieldName: PropTypes.string.isRequired,
+    handleChange: PropTypes.func.isRequired,
+    value: PropTypes.string.isRequired,
+    label: PropTypes.string,
+    isLoading: PropTypes.bool,
+    className: PropTypes.string,
+}
+
+export default FormTypeNumber
