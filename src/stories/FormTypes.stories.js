f728af4d25566950c7e81f2089f86eba84fe6a15
Sefirosweb <sefirosweb@gmail.com>
2022-03-02 18:52:02 +0000

    Added number type in story

--- src/stories/FormTypes.stories.js
+++ src/stories/FormTypes.stories.js
@@ -11,9 +11,18 @@ const Template = (args) => <FormTypes {...args} />
 export const Text = Template.bind({})
 Text.args = {
     type: 'text',
-    selectOptionsUrl: '',
-    inputFieldName: 'input_field_name',
+    inputFieldName: 'input_field_name_text',
     label: 'Insert text',
+    isLoading: false,
+    handleChange: (e) => console.log(e),
+    value: '',
+}
+
+export const Number = Template.bind({})
+Number.args = {
+    type: 'number',
+    inputFieldName: 'input_field_name_number',
+    label: 'Insert number',
     isLoading: false,
     handleChange: (e) => console.log(e),
     value: '',
@@ -22,9 +31,8 @@ Text.args = {
 export const TextArea = Template.bind({})
 TextArea.args = {
     type: 'textarea',
-    selectOptionsUrl: '',
-    inputFieldName: 'input_field_name',
-    label: 'Insert text',
+    inputFieldName: 'input_field_name_textarea',
+    label: 'Insert text area',
     isLoading: false,
     handleChange: (e) => console.log(e),
     value: '',
@@ -33,9 +41,8 @@ TextArea.args = {
 export const Password = Template.bind({})
 Password.args = {
     type: 'password',
-    selectOptionsUrl: '',
-    inputFieldName: 'input_field_name',
-    label: 'Insert text',
+    inputFieldName: 'input_field_name_password',
+    label: 'Insert password',
     isLoading: false,
     handleChange: (e) => console.log(e),
     value: '',
@@ -45,7 +52,7 @@ export const Select = Template.bind({})
 Select.args = {
     type: 'select',
     selectOptionsUrl: '/api/getSelectOptions',
-    inputFieldName: 'input_field_name',
+    inputFieldName: 'input_field_name_select',
     label: 'Select option',
     isLoading: false,
     handleChange: (e) => console.log(e),
@@ -55,8 +62,7 @@ Select.args = {
 export const Checkbox = Template.bind({})
 Checkbox.args = {
     type: 'checkbox',
-    selectOptionsUrl: '',
-    inputFieldName: 'input_field_name',
+    inputFieldName: 'input_field_name_checkbox',
     label: 'Check option',
     isLoading: false,
     handleChange: (e) => console.log(e),
