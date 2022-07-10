cdc405c03626235e84088fde8b3d0af09d800b67
Sefirosweb <sefirosweb@gmail.com>
2022-07-10 15:24:45 +0200

    Refactorizing to typescript

--- example/index.tsx
+++ example/index.tsx
@@ -1,12 +1,14 @@
 import 'react-app-polyfill/ie11';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Thing } from '../.';
+import { Button } from '../.';
 
 const App = () => {
   return (
     <div>
-      <Thing />
+      <Button variant='primary'>
+        s
+      </Button>
     </div>
   );
 };
--- src/Button.tsx
+++ src/Button.tsx
@@ -1,18 +1,19 @@
-import React, { HTMLAttributes, ReactNode } from "react"
+import React, { HTMLAttributes, ReactNode } from 'react';
 
 export interface Props extends HTMLAttributes<HTMLButtonElement> {
-    children: ReactNode;
-    variant: 'primary' | 'secondary'
+  children: ReactNode;
+  variant: 'primary' | 'secondary';
 }
 
-export const Button = (props: Props) => {
-    const { children = 'primary', variant } = props
-    return (
-        <button
-            {...props}
-            style={{ backgroundColor: variant === 'primary' ? 'blue' : 'red' }}
-        >
-            {children}
-        </button>
-    )
+export function Button(props: Props) {
+  const { children = 'primary', variant } = props;
+  return (
+    <button
+      {...props}
+      style={{ backgroundColor: variant === 'primary' ? 'blue' : 'red' }}
+    >
+      asd
+      {children}
+    </button>
+  );
 }
