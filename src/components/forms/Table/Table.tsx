387c513923acb44f8a2f58ecd1aaeb942bd1d45b
Sefirosweb <sefirosweb@gmail.com>
2022-07-10 16:02:47 +0200

    Installed @tanstack/react-table

--- src/components/forms/Table/Table.tsx
+++ src/components/forms/Table/Table.tsx
@@ -1,21 +1,146 @@
 import React from 'react'
-export interface Props {
 
+import {
+    ColumnDef,
+    flexRender,
+    getCoreRowModel,
+    useReactTable,
+} from '@tanstack/react-table'
+
+type Person = {
+    firstName: string
+    lastName: string
+    age: number
+    visits: number
+    status: string
+    progress: number
 }
+
+const defaultData: Person[] = [
+    {
+        firstName: 'javi',
+        lastName: 'linsley',
+        age: 24,
+        visits: 100,
+        status: 'In Relationship',
+        progress: 50,
+    },
+    {
+        firstName: 'tandy',
+        lastName: 'miller',
+        age: 40,
+        visits: 40,
+        status: 'Single',
+        progress: 80,
+    },
+    {
+        firstName: 'joe',
+        lastName: 'dirte',
+        age: 45,
+        visits: 20,
+        status: 'Complicated',
+        progress: 10,
+    },
+]
+
+const columns: ColumnDef<Person>[] = [
+    {
+        accessorKey: 'firstName',
+        cell: info => info.getValue(),
+        footer: info => info.column.id,
+    },
+    {
+        accessorFn: row => row.lastName,
+        id: 'lastName',
+        cell: info => <i>{info.getValue()}</i>,
+        header: () => <span>Last Name</span>,
+        footer: info => info.column.id,
+    },
+    {
+        accessorKey: 'age',
+        header: () => 'Age',
+        footer: info => info.column.id,
+    },
+    {
+        accessorKey: 'visits',
+        header: () => <span>Visits</span>,
+        footer: info => info.column.id,
+    },
+    {
+        accessorKey: 'status',
+        header: 'Status',
+        footer: info => info.column.id,
+    },
+    {
+        accessorKey: 'progress',
+        header: 'Profile Progress',
+        footer: info => info.column.id,
+    },
+]
 
 export const Table = () => {
+    const [data, setData] = React.useState(() => [...defaultData])
+    const rerender = () => setData(defaultData)
+
+    const table = useReactTable({
+        data,
+        columns,
+        getCoreRowModel: getCoreRowModel(),
+    })
+
     return (
-        <table>
-            <tr>
-                <td>
-                    adsads
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    hola
-                </td>
-            </tr>
-        </table>
+        <div className="p-2">
+            <table>
+                <thead>
+                    {table.getHeaderGroups().map(headerGroup => (
+                        <tr key={headerGroup.id}>
+                            {headerGroup.headers.map(header => (
+                                <th key={header.id}>
+                                    {header.isPlaceholder
+                                        ? null
+                                        : flexRender(
+                                            header.column.columnDef.header,
+                                            header.getContext()
+                                        )}
+                                </th>
+                            ))}
+                        </tr>
+                    ))}
+                </thead>
+                <tbody>
+                    {table.getRowModel().rows.map(row => (
+                        <tr key={row.id}>
+                            {row.getVisibleCells().map(cell => (
+                                <td key={cell.id}>
+                                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                                </td>
+                            ))}
+                        </tr>
+                    ))}
+                </tbody>
+                <tfoot>
+                    {table.getFooterGroups().map(footerGroup => (
+                        <tr key={footerGroup.id}>
+                            {footerGroup.headers.map(header => (
+                                <th key={header.id}>
+                                    {header.isPlaceholder
+                                        ? null
+                                        : flexRender(
+                                            header.column.columnDef.footer,
+                                            header.getContext()
+                                        )}
+                                </th>
+                            ))}
+                        </tr>
+                    ))}
+                </tfoot>
+            </table>
+            <div className="h-4" />
+            <button onClick={() => rerender()} className="border p-2">
+                Rerender
+            </button>
+        </div>
     )
 }
+
+
