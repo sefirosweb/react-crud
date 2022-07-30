400ffec7115363de01885e7cc9d72e2bb6cd434f
Sefirosweb <sefirosweb@gmail.com>
2022-07-30 20:46:17 +0200

    pixed postcss

--- .storybook/main.js
+++ .storybook/main.js
@@ -6,7 +6,8 @@ module.exports = {
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
+    "@storybook/addon-interactions",
+    "@storybook/preset-scss"
   ],
   "framework": "@storybook/react",
   // https://storybook.js.org/docs/react/configure/typescript#mainjs-configuration
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,4 @@
+import "../scss/app.scss"
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
