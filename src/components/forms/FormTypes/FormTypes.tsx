db8418d05505cce4a42033394be639f5a4870850
Sefirosweb <sefirosweb@gmail.com>
2022-08-05 13:22:48 +0200

    Refactorized

--- src/components/forms/FormTypes/FormTypes.tsx
+++ src/components/forms/FormTypes/FormTypes.tsx
@@ -10,6 +10,49 @@ import { FieldTypes, SelectOption } from "../../../types";
 
 type ValidFieldTypes = Exclude<FieldTypes, FieldTypes.MULTISELECT>;
 
+type PropsParams = {
+  text: {
+    value?: string;
+    options?: never;
+    selectOptionsUrl?: never;
+  };
+  number: {
+    value?: string | number;
+    options?: never;
+    selectOptionsUrl?: never;
+  };
+  date: {
+    value?: string;
+    options?: never;
+    selectOptionsUrl?: never;
+  };
+  textarea: {
+    value?: string;
+    options?: never;
+    selectOptionsUrl?: never;
+  };
+  password: {
+    value?: string;
+    options?: never;
+    selectOptionsUrl?: never;
+  };
+  checkbox: {
+    value?: string | boolean | number;
+    options?: never;
+    selectOptionsUrl?: never;
+  };
+  multiselect: {
+    value?: string;
+    options?: never;
+    selectOptionsUrl?: never;
+  };
+  select: {
+    value?: string | number | SelectOption;
+    options?: SelectOption[] | string[];
+    selectOptionsUrl?: string;
+  };
+};
+
 type Props<Field extends ValidFieldTypes = ValidFieldTypes> = {
   [field in Field]: {
     type: field;
@@ -18,40 +61,7 @@ type Props<Field extends ValidFieldTypes = ValidFieldTypes> = {
     label?: string;
     isLoading?: boolean;
     handleChange?: any;
-
-    value?: {
-      text: string;
-      number: string | number;
-      date: string;
-      textarea: string;
-      password: string;
-      checkbox: string | boolean | number;
-      multiselect: string;
-      select: string | number | SelectOption;
-    }[field];
-
-    options?: {
-      text: never;
-      number: never;
-      date: never;
-      textarea: never;
-      password: never;
-      checkbox: never;
-      multiselect: never;
-      select: SelectOption[] | string[];
-    }[field];
-
-    selectOptionsUrl?: {
-      text: never;
-      number: never;
-      date: never;
-      textarea: never;
-      password: never;
-      checkbox: never;
-      multiselect: never;
-      select: string;
-    }[field];
-  };
+  } & PropsParams[field];
 }[Field];
 
 function hasField<F extends FieldTypes>(
