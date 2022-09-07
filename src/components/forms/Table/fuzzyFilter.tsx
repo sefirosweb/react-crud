df23cad09609aa307b7027902285f8c383072b69
Sefirosweb <sefirosweb@gmail.com>
2022-09-07 14:34:21 +0200

    Changed method to filter

--- src/components/forms/Table/fuzzyFilter.tsx
+++ src/components/forms/Table/fuzzyFilter.tsx
@@ -1,10 +1,32 @@
-import { FilterFn } from '@tanstack/react-table';
-import { rankItem } from '@tanstack/match-sorter-utils';
+import { FilterFn } from "@tanstack/react-table";
+// import { rankItem } from "@tanstack/match-sorter-utils";
 
-export const fuzzyFilter: FilterFn<any> = (row, columnId, value, addMeta) => {
-  const itemRank = rankItem(row.getValue(columnId), value);
-  addMeta({
-    itemRank,
-  });
-  return itemRank.passed;
+export const fuzzyFilter: FilterFn<any> = (
+  row,
+  columnId,
+  value
+  /*, addMeta*/
+) => {
+  // const itemRank = rankItem(row.getValue(columnId), value);
+  // addMeta({
+  //   itemRank,
+  // });
+  // return itemRank.passed;
+  const text = row.getValue(columnId) as string;
+
+  const result = text
+    .toString()
+    .toLowerCase()
+    .normalize("NFD")
+    .replace(/[\u0300-\u036f]/g, "")
+    .match(
+      value
+        .toString()
+        .toLowerCase()
+        .normalize("NFD")
+        .replace(/[\u0300-\u036f]/g, "")
+    );
+
+  if (result) return true;
+  return false;
 };
