c3791200f43ff5474839ebc6a6a5c18a762719dc
Sefirosweb <sefirosweb@gmail.com>
2022-11-19 20:34:57 +0100

    adding folder test to make a native react app tests

---
+++ test/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { CancelButton } from '@sefirosweb/react-crud'
+function App() {
+  return (
+    <div>
+      Testing app
+      <CancelButton />
+
+    </div>
+  );
+}
+
+export default App;
