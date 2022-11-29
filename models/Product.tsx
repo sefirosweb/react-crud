d53d18b1d7034edccd914f73ca2949cba552d809
Sefirosweb <sefirosweb@gmail.com>
2022-11-29 09:41:41 +0000

    Fixed modal standard modal crud

--- models/Product.tsx
+++ models/Product.tsx
@@ -27,32 +27,44 @@ export type TestModel = {
   input_text_area: string;
 };
 
-export const TestColumnsModel: ColumnDefinition<TestModel>[] = [
+const today = new Date();
+const dd = String(today.getDate()).padStart(2, "0");
+const mm = String(today.getMonth() + 1).padStart(2, "0");
+const yyyy = today.getFullYear();
+
+const fullDatetoday = yyyy + "-" + mm + "-" + dd;
+
+export const TestColumnsModel: Array<ColumnDefinition<TestModel>> = [
   {
     accessorKey: "id",
+    data: "5"
   },
   {
     titleOnCRUD: "Item Type",
     accessorKey: "name",
     editable: true,
+    data: "Core I7 10º gen"
   },
   {
     titleOnCRUD: "API KEY",
     accessorKey: "api_key",
     fieldType: FieldTypes.PASSWORD,
     editable: true,
+    data: "·$ADADFTWSEGF%G%Gg45",
   },
   {
     titleOnCRUD: "Insert number",
     accessorKey: "number_type_field",
     fieldType: FieldTypes.NUMBER,
     editable: true,
+    data: "4412",
   },
   {
     titleOnCRUD: "Insert Date",
     accessorKey: "number_type_date",
     fieldType: FieldTypes.DATE,
     editable: true,
+    data: fullDatetoday
   },
   {
     titleOnCRUD: "Select option",
@@ -60,17 +72,20 @@ export const TestColumnsModel: ColumnDefinition<TestModel>[] = [
     fieldType: FieldTypes.SELECT,
     editable: true,
     selectOptionsUrl: "/api/get_options",
+    data: "1"
   },
   {
     titleOnCRUD: "Boolean option",
     accessorKey: "input_check_option",
     fieldType: FieldTypes.CHECKBOX,
     editable: true,
+    data: "true",
   },
   {
     titleOnCRUD: "Text area",
     accessorKey: "input_text_area",
     fieldType: FieldTypes.TEXTAREA,
     editable: true,
+    data: "This is a text area",
   },
 ];
--- stories/ModalCrud/StandardModal.tsx
+++ stories/ModalCrud/StandardModal.tsx
@@ -1,21 +1,18 @@
 import React, { useState } from "react";
 import {
-  CrudType,
   ModalCrud,
-  ModalData,
   Props,
 } from "../../src/module/components/forms/ModalCrud";
 import { Button } from "react-bootstrap";
 import { Story } from "@storybook/react";
-import { generateOptionsValue } from "../../.storybook/dataMock";
 import { TestColumnsModel } from "../../models/Product";
 import { AxiosResponse } from "axios";
+import { CrudType } from "../../src/module/types";
 
 const Template = (props: Props) => {
   const { accept, fields } = props;
   const [show, setShow] = useState(false);
 
-  const [modalData, setModalData] = useState<ModalData>({});
   const [crud, setCrud] = useState<CrudType>("CREATE");
 
   const handleSuccess = (request: AxiosResponse<any, any>, crud: string) => {
@@ -31,7 +28,7 @@ const Template = (props: Props) => {
         <Button
           variant="success"
           onClick={() => {
-            if (accept === "XXX") {
+            if (!accept) {
               setAcceptButton("Create");
             } else {
               setAcceptButton(accept);
@@ -48,14 +45,7 @@ const Template = (props: Props) => {
         <Button
           variant="warning"
           onClick={() => {
-            const today = new Date();
-            const dd = String(today.getDate()).padStart(2, "0");
-            const mm = String(today.getMonth() + 1).padStart(2, "0");
-            const yyyy = today.getFullYear();
-
-            const fullDatetoday = yyyy + "-" + mm + "-" + dd;
-
-            if (accept === "XXX") {
+            if (!accept) {
               setAcceptButton("Update");
             } else {
               setAcceptButton(accept);
@@ -63,16 +53,6 @@ const Template = (props: Props) => {
 
             setCrud("UPDATE");
             setShow(true);
-            setModalData({
-              id: "5",
-              name: "Core I7 10º gen",
-              api_key: "·$ADADFTWSEGF%G%Gg45",
-              number_type_field: "4412",
-              input_text_area: "This is a text area",
-              number_type_date: fullDatetoday,
-              input_check_option: "true",
-              input_select_option: generateOptionsValue()[0]["value"],
-            });
           }}
         >
           Update
@@ -82,7 +62,7 @@ const Template = (props: Props) => {
         <Button
           variant="danger"
           onClick={() => {
-            if (accept === "XXX") {
+            if (!accept) {
               setAcceptButton("Delete");
             } else {
               setAcceptButton(accept);
@@ -90,10 +70,7 @@ const Template = (props: Props) => {
 
             setCrud("DELETE");
             setShow(true);
-            setModalData({
-              id: "5",
-              name: "Core I7 10º gen",
-            });
+
           }}
         >
           Delete
@@ -106,7 +83,6 @@ const Template = (props: Props) => {
         setShow={setShow}
         accept={acceptButton}
         crud={crud}
-        data={modalData}
         url={"/api/crud"}
         primaryKey="id"
         handleSuccess={handleSuccess}
@@ -119,6 +95,6 @@ const Template = (props: Props) => {
 
 export const StandardModal: Story<Props> = Template.bind({});
 StandardModal.args = {
-  accept: "XXX",
+  accept: undefined,
   fields: TestColumnsModel,
 };
