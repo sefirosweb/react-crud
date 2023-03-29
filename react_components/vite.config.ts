3bdc3fefe82cb6c09427b6cdc1e4247c669ee2a6
Sefirosweb <sefirosweb@gmail.com>
2023-03-30 00:03:20 +0200

    Externalize packages

--- react_components/vite.config.ts
+++ react_components/vite.config.ts
@@ -3,6 +3,12 @@ import react from '@vitejs/plugin-react-swc'
 import path from 'path'
 import dts from 'vite-plugin-dts'
 
+import { peerDependencies } from './package.json';
+
+function externalPackages(deps: Record<string, string>) {
+  return Object.keys(deps)
+}
+
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
@@ -15,17 +21,30 @@ export default defineConfig({
       external: [
         'react',
         'react-dom',
-        '@tanstack/react-query'
+        '@tanstack/react-query',
+        ...externalPackages(peerDependencies),
       ],
       output: {
         globals: {
           'react': 'React',
           'react-dom': 'ReactDom',
           '@tanstack/react-query': 'TtanstackReactQuery',
+          'react-bootstrap': 'TableBootstrap',
+          'react-i18next': 'reactI18next',
+          'quill': 'Quill',
+          'axios': 'Axios',
+          '@tanstack/react-table': 'reactTable',
+          'i18next': 'i18n',
+          'i18next-browser-languagedetector': 'LanguageDetector',
+          'luxon': 'luxon',
+          'toastr': 'toastr',
+          'xlsx': 'xlsx',
+          '@faker-js/faker': 'faker',
+          'axios-mock-adapter': 'MockAdapter',
         },
       },
     },
-    sourcemap: true,
+    sourcemap: false,
     emptyOutDir: true,
   },
   plugins: [react(), dts(),
