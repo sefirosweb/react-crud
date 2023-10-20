d54cc3fb481730251dcd96aee635f13e0ad5d7c3
Sefirosweb <sefirosweb@gmail.com>
2023-10-20 18:24:55 +0200

    Enable to send custom query client

--- src/api/useGetQueryClient.ts
+++ src/api/useGetQueryClient.ts
@@ -2,7 +2,12 @@ import { QueryClient } from "@tanstack/react-query"
 
 let queryClient: QueryClient | undefined
 
-export const useGetQueryClient = () => {
+export const useGetQueryClient = (customQueryClient?: QueryClient) => {
+    if (customQueryClient) {
+        queryClient = customQueryClient
+        return customQueryClient
+    }
+
     if (!queryClient) {
         queryClient = new QueryClient()
     }
